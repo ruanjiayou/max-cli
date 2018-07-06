07c0e204f08ac64298b383c62a68a2df92dd4b73
ruanjiayou <1439120442@qq.com>
2018-07-06 10:38:03 +0800

    改进;自己处理命令行参数

--- README.md
+++ README.md
@@ -1,28 +1,39 @@
 # max-cli
+```
 定制自己的脚手架,提高工作效率(windows里用git-bash不要用cmd!)
+npm install max-cli -g
 ```
-
+### 使用说明
 ```
-- 下载指定文件/GitHub仓库 URL/filename/dir
-- 生成cache/刷新 可选选项
+命令行里的*有毒
+帮助手册
+  max-cli -h 或者 max-cli --help
+初始化一个项目:
+  max-cli init project_name 
+安装配置:
+  max-cli install
+(进入项目目录)添加配置:
+  max-cli add shttp https://github.com/ruanjiayou/net/index.js src/libs/shttp.js
+更新配置:
+  max-cli update shttp
+移除配置:
+  max-cli remove shttp
+列出所有配置:
+  max-cli list
 ```
-commander.js: 可以自动的解析命令和参数: 用于处理用户输入的命令。
-download-git-repo: 下载并提取 git 仓库: 用于下载项目模板。
+### 引用库说明
+```
 Inquirer.js: 通用的命令行用户界面集合: 用于和用户进行交互。
-handlebars.js: 模板引擎: 将用户提交的信息动态填充到文件中。
-ora: 下载过程久的话: 可以用于显示下载中的动画效果。
 chalk: 可以给终端的字体加上颜色。
 log-symbols: 可以在终端上显示出 √ 或 × 等的图标。
 ```
-- TODO:package.json没有生成
-
-2018-6-28 00:50:40
-- 直接下载git文件
-
-2018-6-29 00:06:29
-
 2018-6-29 00:56:45
 ```
   不用TJ的commander 自己处理process.argv伪数组
-  command('cmd <name>', 'description').action(function)
+  command('cmd <name>').description('description').action(function)
+```
+2018-7-6 10:31:07
+```
+  下载文件有时会是socket hang up
+  -g安装有问题
 ```
--- index.js
+++ index.js
@@ -1,147 +1,145 @@
-const fs = require('fs');
 const path = require('path');
-const program = require('commander');
-const handlebars = require('handlebars');
+const IO = require('./lib/io');
+const hinter = require('./lib/hinter');
+const program = require('./lib/program');
+const configs = require('./config');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const symbols = require('log-symbols');
-// 默认配置文件
-const IO = require('./lib/io');
-const configs = require('./config');
-const dir = process.cwd();
-// 下载git文件的封装
-//const getHTML = require('./lib/getHTML');
-const url = require('url');
 
-async function handleOne(giturl, dest) {
-  let href = url.parse(giturl);
-  // 加个master目录
-  let paths = href.pathname.replace(/^[/]/, '').replace(/[/]$/, '').split('/');
-  paths.splice(2, 0, 'master');
-  // 路由映射
-  let redirect = url.format({
-    protocol: 'https:',
-    host: 'raw.githubusercontent.com',
-    pathname: paths.join('/'),
-    search: href.search,
-    hash: href.hash
-  });
-  const res = await getHTML(redirect);
-  if (res.status !== 'success') {
-    console.log(symbols.error, `下载 ${giturl} 失败!`);
-  } else {
-    console.log(symbols.success, `下载 ${giturl} 成功!`);
-    IO.writeTxt(dest, res.message);
-  }
-}
+// 参数列表
+const args = process.argv.map((it) => { return it; });
+// node路径
+const NODE_PATH = args.shift();
+// 模块全局路径
+const MODULE_PATH = args.shift();
+// 当前执行目录
+const CWD_PATH = process.cwd();
+// 配置文件路径
+const CONFIG_PATH_INFO = path.parse(`${CWD_PATH}/.vscode/.max.json`);
+CONFIG_PATH_INFO.path = `${CWD_PATH}/.vscode/.max.json`;
 
-program.on('--help', function () {
-  console.log('有问题联系我: 1439120442@qq.com');
-});
-/**
- * 初始化项目
- */
 program
-  .command('init <name>', '创建文件夹,初始化一个空的项目')
-  .action(async (name) => {
-    if (!IO.isDirExists(`${dir}/${name}`)) {
-      IO.mkdirs(`${dir}/${name}/.vscode`);
-      IO.writeTxt('.vscode/.max.js', JSON.stringify(configs));
-      const answers = await inquirer.prompt([
-        {
-          name: 'description',
-          message: '请输入项目描述'
-        },
-        {
-          name: 'author',
-          message: '请输入作者名称'
-        }
-      ]);
-      const fileName = `${dir}/${name}/package.json`;
-      const meta = {
-        name,
-        description: answers.description,
-        author: answers.author
-      }
-      if (IO.isFileExists(fileName)) {
-        const content = IO.readTxt(fileName);
-        const result = handlebars.compile(content)(meta);
-        IO.writeTxt(fileName, result);
-      }
-      console.log(symbols.success, chalk.green('项目初始化完成'));
-    } else {
-      // 错误提示项目已存在，避免覆盖原有项目
-      console.log(symbols.error, chalk.red('项目已存在'));
+  .on('-h --help', '帮助手册', () => {
+    console.log('  有问题联系我: 1439120442@qq.com');
+    for (let i = 0; i < program.manual.length; i++) {
+      let manual = program.manual[i];
+      let str = program.padding(manual.name, program.Length + 2);
+      console.log(`${chalk.green(str)}: ${manual.desc}`);
     }
   });
 
 program
-  .command('update <cfg>', '更新指定配置或全部配置')
-  .action(async (cfg) => {
+  .command('init <name>')
+  .description('创建文件夹,初始化一个空的项目')
+  .action(async function (name) {
+    //console.log(arguments, 'arguments');// {0:'a'} arguments
+    //console.log(name, 'name');// a name
+    const project_path = `${CWD_PATH}/${name}`;
+    if (IO.isDirExists(project_path)) {
+      hinter('ProjectExisted', name);
+    } else {
+      const answers = await inquirer.prompt([
+        { name: 'description', message: '请输入项目描述' },
+        { name: 'author', message: '请输入作者名称' }
+      ]);
+      const filepath = `${project_path}/package.json`;
+      const meta = {
+        name,
+        author: answers.author,
+        description: answers.description
+      };
+      IO.mkdirs(project_path);
+      const content = IO.readTxt(`${__dirname}/template/default.hbs`);
+      IO.writeTxt(filepath, program.compile(content, meta));
+      console.log(symbols.success, chalk.green('项目初始化完成!'));
+    }
+    if (!IO.isFileExists(`${project_path}/.vscode`)) {
+      IO.mkdirs(`${project_path}/.vscode`);
+      IO.writeTxt(`${project_path}/.vscode/.max.json`, JSON.stringify(configs, null, 2));
+      console.log(symbols.success, chalk.green('初始配置已写入!'));
+    } else {
+      hinter('ConfigExisted', '.vscode/.max.json');
+    }
+  });
+
+program
+  .command('install')
+  .description('添加初始化配置')
+  .action(function () {
+    if (!IO.isFileExists(CONFIG_PATH_INFO.path)) {
+      program.initConfig(CONFIG_PATH_INFO, configs);
+    } else {
+      hinter('ConfigExisted', '.vscode/.max.json');
+    }
+  });
+
+program
+  .command('update <cfg>')
+  .description('更新指定配置或全部配置')
+  .action(async function (cfg) {
+    program.initConfig(CONFIG_PATH_INFO, configs);
     /**
-     * 空参数或*: 更新所有config
      * 其他参数: 更新指定config
      * 
      * 项目config文件不存在: 新建
      * 添加config
      * 删除config
      */
-    const filepath = dir + '/.vscode/.max.js';
-    if (IO.isFileExists(filepath)) {
-      let str = IO.readTxt(filepath);
-      const temp_config = JSON.parse(str);
-      if (cfg === '*' || cfg === 'all') {
-        for (let k in temp_config) {
-          await handleOne(temp_config[k].origin, path.join(dir, temp_config[k].dest));
-        }
-      } else if (typeof temp_config[cfg] === 'object') {
-        await handleOne(temp_config[cfg].origin, path.join(dir, temp_config[cfg].dest));
-      } else {
-        console.log(symbols.error, chalk.red('无效的配置项!'));
+    let temp_config = JSON.parse(IO.readTxt(CONFIG_PATH_INFO.path));
+    if (cfg === 'all') {
+      for (let k in temp_config) {
+        let temp = temp_config[k];
+        await program.down(temp.origin, path.join(CWD_PATH, temp.dest));
       }
+    } else if (typeof temp_config[cfg] === 'object') {
+      await program.down(temp_config[cfg].origin, path.join(CWD_PATH, temp_config[cfg].dest));
+    } else {
+      hinter('InvalidConfig', 'cfg');
     }
   });
 
 program
-  .command('add <name> <url> <path>', '添加新的配置')
-  .action(async (name, url, path) => {
-    const filepath = dir + '/.vscode/.max.js';
-    if (IO.isFileExists(filepath)) {
-      let str = IO.readTxt(filepath);
-      const temp_config = JSON.parse(str);
-      temp_config[name] = { origin: url, dest: path };
-      str = JSON.stringify(temp_config);
-      IO.writeTxt(filepath, str);
-      await handleOne(url, path.join(dir, path));
-      console.log(symbols.success, chalk.green(`添加配置 ${name} 成功!`));
-    }
+  .command('add <name> <url> <filepath>')
+  .description('添加新的配置')
+  .action(async function (name, url, filepath) {
+    program.initConfig(CONFIG_PATH_INFO, configs);
+    let temp_config = JSON.parse(IO.readTxt(CONFIG_PATH_INFO.path));
+    temp_config[name] = { origin: url, dest: filepath };
+    str = JSON.stringify(temp_config, null, 2);
+    IO.writeTxt(CONFIG_PATH_INFO.path, str);
+    await program.down(url, path.join(CWD_PATH, filepath));
+    console.log(symbols.success, chalk.green(`添加配置 ${name} 成功!`));
   });
 
 program
-  .command('remove <name>', '删除配置')
-  .action((name) => {
-    const filepath = dir + '/.vscode/.max.js';
-    if (IO.isFileExists(filepath)) {
-      let str = IO.readTxt();
-      const temp_config = JSON.parse(str);
-      if (typeof temp_config[name] === 'object') {
-        IO.delFile(path.join(dir, temp_config[name].dest));
-        delete temp_config[name];
-        str = JSON.stringify(temp_config);
-        IO.writeTxt(filepath, str);
-        console.log(symbols.success, chalk.green(`移除配置 ${name} 成功!`));
-      } else {
-        console.log(symbols.error, chalk.red(`${name} 配置不存在!`));
-      }
+  .command('remove <name>')
+  .description('删除配置')
+  .action(function (name) {
+    program.initConfig(CONFIG_PATH_INFO, configs);
+    let temp_config = JSON.parse(IO.readTxt(CONFIG_PATH_INFO.path));
+    let filepath = path.join(CWD_PATH, temp_config[name].dest);
+    delete temp_config[name];
+    IO.delFile(filepath);
+    IO.writeTxt(CONFIG_PATH_INFO.path, JSON.stringify(temp_config, null, 2));
+    console.log(symbols.success, chalk.green(`移除配置 ${name} 成功!`));
+  });
+
+program
+  .command('list')
+  .description('列出所有配置')
+  .action(function () {
+    console.log('');
+    console.log(chalk.red('配置列表'));
+    program.initConfig(CONFIG_PATH_INFO, configs);
+    let temp_config = JSON.parse(IO.readTxt(CONFIG_PATH_INFO.path));
+    let i = 0;
+    for (let k in temp_config) {
+      i++;
+      console.log(chalk.green(` ${i}. ${k}:`));
+      console.log('    公网地址:' + temp_config[k].origin);
+      console.log('    本地路径:' + path.join(CWD_PATH, temp_config[k].dest));
     }
   });
-console.log(process.argv);
-//const argv = Array.prototype.shift.call(process.argv);
-// console.log(argv);
-// program
-//   .command('*')
-//   .action(function (env) {
-//     console.log('deploying "%s"', env);
-//   });
 
-program.parse(process.argv);
+program.parse(args);
